841817ba15dedc60bb172ce24ffc925a7b1d1d40
Valerii <tehbrut@gmail.com>
2026-05-05 20:14:15 +0200

    fix: initialize EncodeParams::metadata to silence GCC -Werror=missing-field-initializers

--- tests/test_phase8_9.cpp
+++ tests/test_phase8_9.cpp
@@ -41,6 +41,7 @@ static EncodeParams make_params(CompressionAlgo algo = CompressionAlgo::Identity
         .timestamp = 0,
         .format_id = 0x0001,
         .filename  = "test.bin",
+        .metadata  = {},
     };
 }
 
